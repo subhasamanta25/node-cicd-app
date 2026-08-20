da67ab77b0996d441ef0e9c71b93a4cbe3c8c04b
subhasamanta25 <subhasamanta33964@gmail.com>
2026-08-20 05:51:09 +0000

    edited by subha

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Node.js app edoited by Santu" });
+  res.json({ message: "Hello from Node.js app" });
 });
 
 app.get("/health", (req, res) => {
