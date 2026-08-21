b43ca85cbcce1b6b9189b9012faa6d9205369b67
subhasamanta25 <subhasamanta33964@gmail.com>
2026-08-21 08:26:17 +0000

    Edited by Santu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Node.js app created by Subha" });
+  res.json({ message: "Hello from Node.js app created by Santu" });
 });
 
 app.get("/health", (req, res) => {
