83b36273f768bc2783c06054af8f8473c876b399
subhasamanta25 <subhasamanta33964@gmail.com>
2026-09-21 04:16:39 +0000

    Edited by subha

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/about", (req, res) => {
 if (require.main === module) {
   const PORT = process.env.PORT || 3000;
 
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 }
